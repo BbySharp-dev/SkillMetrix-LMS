b062a8ba5b9fd8f57ae8e8fedc6ceec9a2a812fd
BbySharp-dev <doanminhtruong53.dev@gmail.com>
2026-04-23 15:19:48 +0700

    feat(client): implement advanced Axios auth interceptor and token rotation

---
+++ client/src/api/authApi.ts
@@ -0,0 +1,17 @@
+import api from "./axios.ts";
+import type {AuthPayload, LoginRequest, RegisterRequest} from "../types/auth.ts";
+
+
+export const authApi = {
+    login: async (payload: LoginRequest): Promise<AuthPayload> => {
+        const res = await api.post('/auth/login' , payload);
+        return res.data.data as AuthPayload;
+    },
+    register: async (payload: RegisterRequest): Promise<AuthPayload> => {
+        const res = await api.post('/auth/register' , payload);
+        return res.data.data as AuthPayload;
+    },
+    logout: async (refreshToken: string): Promise<void> => {
+        await api.post('/auth/logout' , {refreshToken});
+    },
+}
