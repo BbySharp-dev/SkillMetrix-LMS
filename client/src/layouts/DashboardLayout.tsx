3adb27ad589a4ec664581e9de41d1f4581444a54
BbySharp-dev <doanminhtruong53.dev@gmail.com>
2026-04-29 22:29:04 +0700

    refactor(layouts): migrate DashboardLayout and MainLayout to use new Header/Footer components with improved spacing and font imports

--- client/src/layouts/DashboardLayout.tsx
+++ client/src/layouts/DashboardLayout.tsx
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom';
-import Header from '@/components/layout/Header';
+import DashboardHeader from '@/components/layout/DashboardHeader';
 import Sidebar from '@/components/layout/Sidebar';
 
 export default function DashboardLayout() {
     return (
-        <div className="min-h-screen flex flex-col">
-            <Header />
-            <div className="flex flex-1">
+        <div className="min-h-screen flex flex-col bg-gray-50/50">
+            <DashboardHeader />
+            <div className="flex flex-1 overflow-hidden">
                 <Sidebar />
-                <main className="flex-1 container mx-auto px-4 py-6">
-                    <Outlet />
+                <main className="flex-1 overflow-y-auto p-4 md:p-8">
+                    <div className="max-w-6xl mx-auto">
+                        <Outlet />
+                    </div>
                 </main>
             </div>
         </div>
